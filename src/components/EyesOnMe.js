44af245d3278d9e88a467f29c2999319d45dba43
DidasMba <daniel.mbarushi@gmail.com>
2023-10-26 19:46:47 +0300

    add

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  // Define event handler functions for the 'focus' and 'blur' events
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div>
+      {/* Render a button with 'Eyes on me' text, and add event handlers */}
+      <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Define an event handler function for the 'change' event
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      {/* Render an input field with the type 'password' */}
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
